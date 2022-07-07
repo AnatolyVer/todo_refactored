0686eeee0e36aad307f08d0848a41bffa4ace702
AnatolyVer <tolik.vereshchak88@gmail.com>
2022-07-07 17:05:07 +0300

    Refactored project "ToDoList"

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from "../Components/Header/Header";
+import Content from "../Components/Content/Content";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
